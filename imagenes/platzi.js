b54abc67380c852060bea5993ebe0640ab6c53d6
ProgramacionDiaz <101899151+ProgramacionDiaz@users.noreply.github.com>
2022-05-20 17:07:13 -0500

    Delete imagenes directory

--- imagenes/platzi.js
+++
@@ -1,9 +0,0 @@
-var z = aleatorio(10, 20);
-document.write(z);
-
-function aleatorio(min, maxi)
-{
-    var resultado;
-    resultado = Math.floor(math.random() * (maxi - min + 1)) + min;
-    return resultado;
-}
