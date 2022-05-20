9fad1d92013c4e4fd357b7dbe8161c0ce3383281
ProgramacionDiaz <101899151+ProgramacionDiaz@users.noreply.github.com>
2022-05-20 17:14:25 -0500

    Add files via upload
    20220325/trabajo en clase

---
+++ 20220325/trabajos/1.php
@@ -0,0 +1,10 @@
+<?php
+//definimos variables en rojo
+$miVariable = "rojo";
+//pregunta si variable es igual a rojo
+if ($miVariable == "rojo") {
+    echo "el color si qeu es rojo "
+} else {
+    echo "el color parece que no coincide";
+}
+?>
---
+++ 20220325/trabajos/david.php
@@ -0,0 +1,9 @@
+<?php
+//definimos una variable como texto
+$miVariable = "hola variable";
+//visualizacion la variable en el explorador 
+echo "<br>";e
+$miValor = 2;
+// visualizamos sin embargo realizamos una multiplicacion * 3
+echo 3*$miValor;
+?>
